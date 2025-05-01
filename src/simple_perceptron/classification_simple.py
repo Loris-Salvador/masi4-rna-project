890f741f5216cc11a37da5a97dba39f342cb2690
Loris Salvador <dev.loris.salvador@gmail.com>
2025-05-01 03:12:06 +0200

    delete heritage

--- src/simple_perceptron/classification_simple.py
+++ src/simple_perceptron/classification_simple.py
@@ -1,14 +1,28 @@
 import numpy as np
 import pandas as pd
-from simple_perceptron.simple_perceptron_base import SimplePerceptronBase
 import matplotlib.pyplot as plt
 
 
 
-class ClassificationSimplePerceptron(SimplePerceptronBase):
+class ClassificationSimplePerceptron():
     
     def __init__(self, data_path):
-        super().__init__(data_path)
+        data = pd.read_csv(data_path, header=None)
+
+        #toutes les lignes et toutes les colonnes sauf derniere
+        self.entries = data.iloc[:, :-1].values
+
+        #toutes les lignes et uniquement la derniere colonne
+        self.exp_outputs = data.iloc[:, -1].values 
+
+        # Ajouter une colonne de 1 pour le biais à self.entries
+        self.entries = np.c_[np.ones((self.entries.shape[0], 1)), self.entries]
+
+        #shape[0] donne le nb lignes et shape[1] le nb colonnes
+        n_features = self.entries.shape[1]
+
+        self.weights = np.random.randn(n_features)
+
 
 
     def __activation(self, z):
